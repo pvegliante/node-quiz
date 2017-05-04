7d21ab98706ee2761daafa6b1fa1c6b3070cbf94
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-05-04 18:16:43 -0400

    second

---
+++ node quiz/quiz.js
@@ -0,0 +1,58 @@
+Node Quiz                                                                                                                Name:_____________
+
+1)	What is node.js and why should we use it?
+
+2)	Does NPM come bundled with node.js when you install it?
+
+3)	What is the function used to send a file to a browser from an express app?
+
+4)	What does the following code do?  app.use(bodyParser.json());
+
+5)	What does the following code do? app.use(express.static('public'));
+
+6)	Please create a basic outline for a CRUD application for the following:
+
+I am your client, and I need an API for a stock market app I am building. Happily, you got the job!!
+
+I need the following data:
+
+-	stock name
+-	stock symbol
+-	stock price
+-	time.date of price
+-	market instrument is traded on
+
+I will need the the full complement of CRUD for my API. Read should include the ability to retrieve all the stocks, or one stock.
+
+Create an outline or plan for the data, and the end points. No need to write the code, but I do want to see the plan that you would implement the API from. Use the following as a model:
+
+
+
+
+7)	What is the “event Loop”?
+
+8)	What is a callback?
+
+9)	Please explain synchronous vs asynchronous behavior.
+
+10)	What is package.json?
+
+11)	Which module is used for file based operations?
+
+12)	How will you open a file using node?
+
+13)	What is the purpose of __dirname variable?
+
+14)	Can you access the DOM in node?
+
+15)	Explain briefly how node.js solves the problem of blocking i/o.
+
+16)	What is the most common used framework used in node.js? Hint:  It’s used to write web servers.
+
+17)	Please write a basic route using the get verb that returns all the items from an array in JSON format. Hint: We did this in class…
+
+18)	What does CRUD stand for?
+
+19)	Who developed node.js?
+
+20)	How do we make code in a file (module) in our project accessible to other modules in our project?  Please describe what you need to do in the module with the code, and in the module that wishes to use that code.
